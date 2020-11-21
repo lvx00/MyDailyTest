b60a9950b95774c8fd3e74cd3542ab4a337b762a
lvx00 <18335094427@163.com>
2020-11-21 15:10:34 +0800

    第一个git

--- src/com/saunfa/MyQuck.java
+++ src/com/saunfa/MyQuck.java
@@ -1,2 +1,61 @@
-package com.saunfa;public class MyQuck {
+package com.saunfa;
+
+import java.util.Arrays;
+
+public class MyQuck {
+
+    public static void main(String[] args) {
+        int a[] = {4, 5, 3, 5, 6, 7, 2, 7, 8, 2, 6, 5, 6, -13834, -349, 434024, -211414};
+        int[] ints = new MyQuck().quick(a, 0, a.length);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public int[] quick(int a[], int begin, int end) {
+        if (end - begin < 2) return a;
+
+        int mid = sort(a, begin, end);
+
+        // 左边递归
+        sort(a, begin, mid);
+        // 右边递归
+        sort(a, mid + 1, end);
+
+        return a;
+    }
+
+    public int sort(int a[], int begin, int end) {
+
+        int cur = a[begin];
+        end--;
+
+        while (begin < end) {
+
+            while (begin < end) {
+                // 从后面开始遍历
+                if (a[end] > cur) {
+                    end--;
+                } else {
+                    a[begin] = a[end];
+                    begin++;
+                    break;
+                }
+            }
+
+            while (begin < end) {
+                // 从前边开始遍历
+                if (a[begin] < cur) {
+                    begin++;
+                } else {
+                    a[end] = a[begin];
+                    end--;
+                    break;
+                }
+            }
+
+
+        }
+        a[begin] = cur;
+
+        return begin;
+    }
 }
--- src/com/saunfa/MySort.java
+++ src/com/saunfa/MySort.java
@@ -1,5 +1,7 @@
 package com.saunfa;
 
+import java.util.Arrays;
+
 public class MySort {
 
     // 冒泡排序
@@ -63,86 +65,15 @@ public class MySort {
         return a;
     }
 
-
     // 希尔排序
     public int[] shell(int[] a) {
-        // 首先根据数组的长度找到增量h
-        int h = 1;
-        while (h < a.length / 2) {
-            h = 2 * h + 1;
-        }
-        // 进行排序
-        while (h >= 1) {
-            // 从前往后，默认前h个元素是前h个组中的有序元素
-            int cur = 0;
-            for (int i = h; i < a.length; i++) {
-
-                if (a[i] > a[i - h]) {
-                    // 如果待插入元素比前面的最后一个元素还大，就不进行操作
-                } else {
-                    // 记录下代插入的元素
-                    cur = a[i];
-                    // 找到待插入元素的位置
-                    int j = i;   //辅助位置
-                    while (j - h > 0) {
-                        if (a[j - h] > a[j]) {
-                            a[j] = a[j - h];
-                        } else {
-                            break;
-                        }
-                        j -= h;
-                    }
-                    a[j] = cur;
-                }
-            }
-
-            h = h / 2;
-        }
-        return a;
-    }
-
-
-    // 希尔排序测试
-    public int[] shell1(int[] a) {
-        // 首先通过数组的长度确定增量h
-        int h = 1;
-        while (h < a.length / 2) {
-            h = h * 2 + 1;
-        }
-        while (h >= 1) {
-            // 将元素分成了h组，第h个元素就是我们要插入的地方
-            for (int i = h; i < a.length; i++) {
-                if (a[i] > a[i - h]) {
-                    // 如果待插入元素比前面有序的那个数组大，就不需要进行操作了
-                } else {
-                    // 否则的话就需要对前面的数组和待插入的元素进行比较，找到待插入元素的插入位置
-                    int cur = a[i]; //记录待插入的元素
-                    int j = i;      // 辅助变量
-                    while (j - h > 0) {   // 避免数组越界
-                        if (cur < a[j - h]) { // 如果待插入元素比前面有序数组中的元素小，就让数组后移一位
-                            a[j] = a[j - h];
-                        } else {
-                            // 如果不大于了就说明找到了待插入元素的位置
-                            break;
-                        }
-                        j -= h;
-                    }
-                    a[j] = cur;
-                }
-            }
-            h = h / 2;
-        }
-        return a;
-    }
-
-    // 希尔排序
-    public int[] shell2(int[] a) {
         int h = 1;
         while (h < a.length / 2) {
             h = 2 * h + 1;
         }
         while (h >= 1) {
             for (int i = h; i < a.length; i++) {
+                // 如果待插入的元素比前面有序元素的最后一个大。就不需要做出改变
                 if (a[i] > a[i - h]) {
 
                 } else {
@@ -164,13 +95,10 @@ public class MySort {
         return a;
     }
 
+
     public static void main(String[] args) {
-        int a[] = {1, 6, 12, 65, 86, 2, 8, 8, 34, 3, 54, 57, 78, 89, 3};
-        int[] m = new MySort().shell2(a);
-        for (int i : m) {
-            System.out.println(i);
-        }
-
+        int a[] = {4, 5, 3, 5, 6, 7, 2, 7, 8, 2, 6};
+        int[] m = new MySort().shell(a);
+        System.out.println(Arrays.toString(a));
     }
-
 }
